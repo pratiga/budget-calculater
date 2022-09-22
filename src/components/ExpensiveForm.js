6bd876abcd57f0e729d5bc0f22835d81baa5d7fc
pratiga <gaitrisanu2468@gmail.com>
2022-09-22 20:32:59 +0545

    form component
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/components/ExpensiveForm.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { MdSend } from 'react-icons/md'
+const ExpensiveForm = (
+    {
+        charge,
+        amount,
+        handleAmount,
+        handleCharge,
+        handleSubmit , 
+    }
+) => {
+  return (
+   <form onSubmit={handleSubmit}>
+   <div className='form-center'>
+    <div className='form-group'>
+    <label htmlFor='charge'>charge</label>
+    <input
+        type="text"
+        className='form-control'
+        id='charge'
+        placeholder='e.g. rent'
+        value={charge}
+        onChange={handleCharge}
+    />
+    </div>
+    <div className='form-group'>
+        <label htmlFor = "amount">amount</label>
+    <input
+        type="text"
+        className="form-control"
+        id="amount"
+        name="amount"
+        placeholder="e.g. 100"
+        value={amount}
+        onChange={handleAmount}
+    />
+    </div>
+   </div>
+   <button type="submit" className='btn'>
+    submit
+    <MdSend className="btn-icon" />
+   </button>
+   </form>
+   
+  )
+}
+
+export default ExpensiveForm
