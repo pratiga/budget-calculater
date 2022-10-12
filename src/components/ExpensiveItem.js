4f2810858d64b165fbb87fc50df76e2738000167
pratiga <gaitrisanu2468@gmail.com>
2022-10-12 10:59:08 +0545

    pass parameter id through onclick function
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/ExpensiveItem.js
+++ src/components/ExpensiveItem.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md';
 
-const ExpensiveItem = ({expense}) => {
-    const { id, charge, amount } = expense;
+const ExpensiveItem = ({expense, handleEdit,handleDelete}) => {
+    const { id, charge, amount, } = expense;
   return (
    <li className='item'>
    <div className='info'>
    <span className='expense'>{charge}</span>
    <span className='amount'>${amount}</span>
    </div>
-   <button className='edit-btn' arial-label = "edit-button">
+   <button className='edit-btn' arial-label = "edit-button"
+   onClick = {() => handleEdit(id)}
+   >
     <MdEdit />
    </button>
-   <button className='clear-btn' aria-label='delete-button'>
+   <button className='clear-btn' aria-label='delete-button'
+   onClick={()=> handleDelete(id)}
+   >
     <MdDelete />
    </button>
    </li>
