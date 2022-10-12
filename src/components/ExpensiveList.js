7003d15655ef299004149ff17f17e9385e18404d
pratiga <gaitrisanu2468@gmail.com>
2022-10-12 11:00:00 +0545

    call function of delete edit
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/ExpensiveList.js
+++ src/components/ExpensiveList.js
@@ -2,17 +2,26 @@ import React from 'react'
 import {MdDelete} from "react-icons/md";
 import ExpensiveItem from './ExpensiveItem'
 
-const ExpensiveList = ({expenses}) => {
+const ExpensiveList = ({expenses,handleEdit, handleDelete,clearItem}) => {
   return (
    <>
    <ul>
     {
         expenses.map(expense => {
-            return <ExpensiveItem key={expense.id} expense={expense}/>
+            return <ExpensiveItem
+             key={expense.id}
+             expense={expense}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            />
         })
     }
    </ul>
-   {expenses.length > 0 && <button className='btn'>
+   {expenses.length > 0 && 
+    <button className='btn'
+    onClick={clearItem}
+    >
+    
     clear expenses
     <MdDelete className="btn-icon" />
     </button>
